d76ddbb14e38f2c3a8cd9d1abdec240edc557a02
Mexel70 <mexel70@live.de>
2019-10-02 13:17:07 +0200

    Added solution folders in workspace

--- premake5.lua
+++ premake5.lua
@@ -27,8 +27,12 @@ workspace "Nexus"
 		"Distribution"
 	}
 
+group "Engine/Runtime"
+
 include (RuntimeSourceDirectory .. "Core")
 include (RuntimeSourceDirectory .. "Sandbox")
+
+group "Engine/ThirdParty"
 
 include (ThirdPartySourceDirectory .. "glfw")
 include (ThirdPartySourceDirectory .. "glad")
