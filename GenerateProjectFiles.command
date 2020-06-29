41f3b4acd56ad4d3bffb65b97fcf7281d84596b8
Mexel70 <mexel70@live.de>
2020-06-29 14:51:21 +0200

    Updated script files

--- GenerateProjectFiles.command
+++ GenerateProjectFiles.command
@@ -1,2 +1,3 @@
 #!/bin/sh
+chmod +x Engine/Build/Mac/Premake/premake5
 ./Engine/Build/Mac/Premake/premake5 xcode4
--- GenerateProjectFiles.sh
+++ GenerateProjectFiles.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+chmod u+x Engine/Build/Linux/Premake/premake5
 ./Engine/Build/Linux/Premake/premake5 gmake2
